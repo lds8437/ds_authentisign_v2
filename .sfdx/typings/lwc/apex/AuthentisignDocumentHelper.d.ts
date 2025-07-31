611a221fd7cef8e03c6007d785a8384756295c7d
dlimlwolf <dlim@lwolf.com>
2025-07-31 13:58:32 -0600

    Remove Namespace and Update Apex classes for object flexibility

---
+++ .sfdx/typings/lwc/apex/AuthentisignDocumentHelper.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/AuthentisignDocumentHelper.fillandSubmit" {
+  export default function fillandSubmit(param: {requestString: any, recordId: any, recordName: any, documentId: any, documentName: any, objectName: any}): Promise<any>;
+}
